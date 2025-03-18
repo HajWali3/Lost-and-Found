82ba155650c6a45bdec563f8cb08e26b92107099
Haj Wali <hajwalii3@gmail.com>
2025-03-18 15:37:46 +0500

    utils code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse extends Error {
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+    this.success = statusCode < 400;
+  }
+}
+
+export { ApiResponse };
