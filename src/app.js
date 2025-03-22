a65f845558587b35fc80ad8c76cc208e0f29708e
Haj Wali <hajwalii3@gmail.com>
2025-03-22 18:51:33 +0500

    all setup config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -14,5 +15,18 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.route.js";
+
+// routes declaration
+// app.use("/api/v1/users", userRouter);
+app.use("/users", userRouter);
+
+app.get("/home", (req, res) => {
+  console.log("home");
+
+  res.send("home");
+});
 
 export { app };
